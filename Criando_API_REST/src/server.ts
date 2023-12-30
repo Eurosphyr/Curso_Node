d8a7348a2ad2841da9561c2791673225762ef22c
Eurosphyr <miguel.mmgg2016@gmail.com>
2023-12-30 19:39:07 -0300

    Update .gitignore and .eslintrc.json, configure tsconfig.json, and modify database.ts and package.json

--- Criando_API_REST/src/server.ts
+++ Criando_API_REST/src/server.ts
@@ -1,18 +1,18 @@
-import fastify from 'fastify'
-import { knex } from './database'
+import fastify from "fastify";
+import { knex } from "./database";
 
-const app = fastify()
+const app = fastify();
 
-app.get('/hello', async () => {
-  const test = knex('sqlite_schema').select('*')
+app.get("/hello", async () => {
+  const test = knex("sqlite_schema").select("*");
 
-  return test
-})
+  return test;
+});
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('Server is running on port 3333')
-  })
+    console.log("Server is running on port 3333");
+  });
