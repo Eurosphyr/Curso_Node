2c69c5d4a40dcfec3dc3c1a9d256021e8602d4e6
Eurosphyr <miguel.mmgg2016@gmail.com>
2024-01-13 10:43:32 -0300

    FINALIZADO Teste_Design_Patterns

--- 03-API_SOLID/src/services/register.ts
+++ 03-API_SOLID/src/services/register.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcryptjs";
 import { UserAlreadyExistError } from "./errors/user-already-exist";
 import { UsersRepository } from "@/repositories/users-repository ";
+import { User } from "@prisma/client";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -8,10 +9,18 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -21,10 +30,13 @@ export class RegisterUseCase {
 
     // const prismaUsersRepository = new PrismaUsersRepository();
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+    return {
+      user,
+    };
   }
 }
